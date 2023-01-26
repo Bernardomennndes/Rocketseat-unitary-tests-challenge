c1fdbd540762d9c26b297106afb34a50855546da
Bernardo Mendes <bernardomennndes@outlook.com>
2023-01-25 22:12:57 -0300

    [REFACTOR] Settings adapted to application infrastructure

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from "ts-jest";
+
+import { compilerOptions } from "./tsconfig.json";
+
+export default {
+  bail: true,
+  clearMocks: true,
+  coverageProvider: "v8",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src/",
+  }),
+  preset: "ts-jest",
+  setupFiles: ["dotenv/config", "reflect-metadata"],
+  testEnvironment: "node",
+  testMatch: ["**/*.spec.ts"],
+};
